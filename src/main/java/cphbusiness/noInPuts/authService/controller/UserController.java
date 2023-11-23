590bb8568ac148344a538d04b10dbea120711647
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-11-23 09:24:40 +0100

    Comments, refactoring and removed rabbitmq in usercontroller for now

--- src/main/java/cphbusiness/noInPuts/authService/controller/UserController.java
+++ src/main/java/cphbusiness/noInPuts/authService/controller/UserController.java
@@ -22,13 +22,11 @@ public class UserController {
 
     private final UserService userService;
     private final JwtService jwtService;
-    //private final RabbitMessagePublisher rabbitMessagePublisher;
 
     @Autowired
     public UserController(UserService userService, JwtService jwtService) {
         this.userService = userService;
         this.jwtService = jwtService;
-        //this.rabbitMessagePublisher = rabbitMessagePublisher;
     }
 
     // Endpoint for creating a user account
@@ -41,6 +39,7 @@ public class UserController {
         // Catch exception if user already exists else create entity in DB
         UserDTO userDTO;
         try {
+            // Get user from DB
             userDTO = userService.createUser(POSTuserDTO);
         } catch (UserAlreadyExistsException e) {
             return new ResponseEntity<>(HttpStatus.CONFLICT);
@@ -48,21 +47,12 @@ public class UserController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
-        // Genereate JWT token for user
-        String jwtToken = jwtService.tokenGenerator(userDTO.getId(), userDTO.getUsername(), "user");
-
-        // Send out createdUserEvent to RabbitMQ/Apache Camel
-        //rabbitMessagePublisher.createdUserEvent("User created: " + userDTO.getUsername());
-
-        // Creates response entity with userDTO and JWT token
+        // Creates response entity with userDTO
+        // TODO Remove hashmap and just return userDTO
         Map<String, Object> response = new HashMap<>();
         response.put("user", userDTO);
-        Cookie cookie = new Cookie("jwt-token", jwtToken);
-        //cookie.setHttpOnly(true);
-        cookie.setMaxAge(2 * 24 * 60 * 60);
-        cookie.setPath("/");
 
-        servletResponse.addCookie(cookie);
+        servletResponse.addCookie(getJwtCookie(userDTO.getId(), userDTO.getUsername()));
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
@@ -80,22 +70,12 @@ public class UserController {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
 
-        // Generate JWT token for user
-        String jwtToken = jwtService.tokenGenerator(userDTO.getId(), userDTO.getUsername(), "user");
-
-        // Creates response entity with userDTO and JWT token
+        // Creates response entity with userDTO
+        // TODO Remove hashmap and just return userDTO
         Map<String, Object> response = new HashMap<>();
         response.put("user", userDTO);
 
-        Cookie cookie = new Cookie("jwt-token", jwtToken);
-
-        // cookie expires in 2 days
-        cookie.setMaxAge(2 * 24 * 60 * 60);
-        cookie.setPath("/");
-        // TODO: Set TLS/SSL certificate
-        //cookie.setSecure(true);
-        //cookie.setHttpOnly(true);
-        servletResponse.addCookie(cookie);
+        servletResponse.addCookie(getJwtCookie(userDTO.getId(), userDTO.getUsername()));
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
@@ -104,22 +84,39 @@ public class UserController {
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Object> logout(@Valid @CookieValue("jwt-token") String jwtToken, HttpServletResponse servletResponse) {
 
+        // Check if jwtToken is empty
         if(jwtToken.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
+        // Try to logout user
         try {
             jwtService.logout(jwtToken);
         } catch (AlreadyLoggedOutException e) {
             return new ResponseEntity<>(HttpStatus.OK);
         }
 
+        // Delete jwt-token cookie
         Cookie deleteCookie = new Cookie("jwt-token", null);
         deleteCookie.setMaxAge(0);
         deleteCookie.setPath("/");
-
         servletResponse.addCookie(deleteCookie);
+
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private Cookie getJwtCookie(Long id, String username) {
+        // Generate JWT token for user
+        String jwtToken = jwtService.tokenGenerator(id, username, "user");
+        Cookie cookie = new Cookie("jwt-token", jwtToken);
+        cookie.setMaxAge(2 * 24 * 60 * 60);
+        cookie.setPath("/");
+
+        // TODO: Set TLS/SSL certificate
+        //cookie.setSecure(true);
+        //cookie.setHttpOnly(true);
+
+        return cookie;
     }
 
 }
